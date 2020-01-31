c3b9c21b49a521e98eba8da0a79e0b573e6fb791
marvinhm <marvinmatovu@gmail.com>
2020-01-31 23:47:36 +0000

    initial commit

---
+++ js/src/Application.js
@@ -0,0 +1,12 @@
+class Application {
+  /**
+   * @param {Object} basket
+   * @param {Object} discounts
+   * @returns {!number}
+   */
+  static main(basket, discounts) { // eslint-disable-line no-unused-vars
+    throw new Error('You must implement this.');
+  }
+}
+
+module.exports = Application;
