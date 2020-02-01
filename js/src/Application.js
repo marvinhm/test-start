8248a6408248d6813fb6741f6d66c3e5d55a6b04
marvinhm <marvinmatovu@gmail.com>
2020-02-01 01:22:26 +0000

    returns base price of product without discounts

--- js/src/Application.js
+++ js/src/Application.js
@@ -4,9 +4,12 @@ class Application {
    * @param {Object} discounts
    * @returns {!number}
    */
+
   static main(basket, discounts) { // eslint-disable-line no-unused-vars
     if (Object.keys(basket).length === 0) {
       return 0;
+    } else if (Object.keys(basket).length > 0) {
+      return basket[Object.keys(basket)[0]].price;
     }
     throw new Error('You must implement this.');
   }
