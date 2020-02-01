40608668ae0622e3209e58361ca3b3d36007a1e8
marvinhm <marvinmatovu@gmail.com>
2020-02-01 16:08:15 +0000

    cleanup - added functions to get basket and discount ids

--- js/src/Application.js
+++ js/src/Application.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 class Application {
   /**
    * @param {Object} basket
@@ -5,33 +6,43 @@ class Application {
    * @returns {!number}
    */
 
-  static checkBasketIfInUse(basket) {
-    let priceVal = 0;
-    if (Object.keys(basket).length > 1) {
-      for(let i in basket) {
-        priceVal += basket[i].price * basket[i].quantity;
-      }
-    } else if (Object.keys(basket).length === 1) {
-      priceVal = basket[Object.keys(basket)[0]].price;
-    }
-    return priceVal;
+  static getDiscountIdArray(discounts) {
+    const discountkeysarray = Object.keys(discounts);
+    return discountkeysarray;
+  }
+
+  static getBasketId(basket) {
+    const basketid = Object.keys(basket)[0];
+    return basketid;
   }
 
   static basketQuantity(basket) {
     return Object.keys(basket).length;
   }
 
+  static checkBasketIfInUse(basket) {
+    let priceVal = 0;
+    if (Object.keys(basket).length > 1) {
+      // eslint-disable-next-line guard-for-in
+      for (let i in basket) {
+        priceVal += basket[i].price * basket[i].quantity;
+      }
+    } else if (Object.keys(basket).length === 1) {
+      priceVal = basket[this.getBasketId(basket)].price;
+    }
+    return priceVal;
+  }
+
   static getPercentDiscount(price, discounts, basketquantity) {
-    let discountAmount = 0;
+    let discountamount = 0;
     let itemPrice = price;
-    const discountkeysarray = Object.keys(discounts);
+    const discountkeysarray = this.getDiscountIdArray(discounts);
     const discountminimum = discounts[discountkeysarray[0]].min;
-  
     if (discountkeysarray.length > 0 && basketquantity >= discountminimum) {
       itemPrice *= (discounts[discountkeysarray[0]].value / 100);
-      discountAmount = itemPrice;
+      discountamount = itemPrice;
     }
-    return discountAmount;
+    return discountamount;
   }
 
   // static discountsAvailable(discounts) {
@@ -41,7 +52,7 @@ class Application {
     const price = this.checkBasketIfInUse(basket);
     const basketquantity = this.basketQuantity(basket);
     let discountAmount = 0;
-    if (Object.keys(discounts).length > 0) {
+    if (this.getDiscountIdArray(discounts).length > 0) {
       discountAmount = this.getPercentDiscount(price, discounts, basketquantity);
     }
     const total = price - discountAmount;
