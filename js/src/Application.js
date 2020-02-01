56fe04eea86ae144522537c96c514df74028ba5f
marvinhm <marvinmatovu@gmail.com>
2020-02-01 02:54:15 +0000

    sums product prices without discounts

--- js/src/Application.js
+++ js/src/Application.js
@@ -7,7 +7,11 @@ class Application {
 
   static checkBasketIfInUse(basket) {
     let priceVal = 0;
-    if (Object.keys(basket).length > 0) {
+    if (Object.keys(basket).length > 1) {
+      for(let i in basket) {
+        priceVal += basket[i].price * basket[i].quantity;
+      }
+    } else if (Object.keys(basket).length === 1) {
       priceVal = basket[Object.keys(basket)[0]].price;
     }
     return priceVal;
